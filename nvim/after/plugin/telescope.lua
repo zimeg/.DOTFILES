7728131674b61a307c4039755d1e9a686f89e9b6
Ethan Zimbelman <ethan.zimbelman@me.com>
2023-05-14 00:58:39 -0700

    install telescope fuzzy finder

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,12 @@
+local builtin = require('telescope.builtin')
+
+-- Find files from current directory
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Find files within git repository
+vim.keymap.set('n', '<leader>fa', builtin.git_files, {})
+
+-- TODO: Search for a specific string
+vim.keymap.set('n', '<leader>fs', function()
+    builtin.grep_string({ search = vim.fn.input("> ") });
+end)
--- nvim/lua/zim/packer.lua
+++ nvim/lua/zim/packer.lua
@@ -3,5 +3,9 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'}, {'BurntSushi/ripgrep'} }
+    }
 end)
