40328c133e8ced3950647cd0bcf1bd339c2e6435
Ethan Zimbelman <ethan.zimbelman@me.com>
2023-05-14 17:00:42 -0700

    simplify lua lsp setup

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,11 +2,12 @@ local lsp = require('lsp-zero')
 
 lsp.preset("recommended")
 
--- Starting languages
+-- Lanuage configs
 lsp.ensure_installed({
     'lua_ls',
     'rust_analyzer',
 })
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 -- General mappings
 local cmp = require('cmp')
@@ -22,27 +23,6 @@ lsp.on_attach(function(_client, bufnr)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 end)
-
--- Lua configs
-require'lspconfig'.lua_ls.setup {
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                globals = {'vim'},
-            },
-            workspace = {
-	        checkThirdParty = false,
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-}
 
 -- Initialization
 lsp.setup()
