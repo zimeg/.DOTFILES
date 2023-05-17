97ef453448037f333b4293b01c6e079b7e10a5b9
Ethan Zimbelman <ethan.zimbelman@me.com>
2023-05-16 23:15:12 -0700

    setup a simple statusline

--- nvim/lua/configs/style.lua
+++ nvim/lua/configs/style.lua
@@ -5,3 +5,11 @@ vim.cmd.colorscheme 'melange'
 -- Remove the background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- Customize the status bar
+vim.cmd([[highlight StatusLeft guifg=#b89984]])
+vim.cmd([[highlight StatusRight guifg=#7A6C60]])
+vim.cmd([[
+    set statusline=%#StatusLeft#\%F\ %R
+    set statusline+=\%=
+    set statusline+=\%#StatusRight#\ %M\%l:%c\ ]])
