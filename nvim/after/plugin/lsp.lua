be2dbaff02eecf1d625ca0c8298e57dfa59ed4a9
Ethan Zimbelman <ethan.zimbelman@me.com>
2023-07-28 15:27:01 -0700

    nvim: add language servers for deno and typescript

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,13 +5,22 @@ local cmp = require('cmp')
 -- Lanuage configs
 lsp.preset("recommended")
 lsp.ensure_installed({
+    'denols',
     'gopls',
     'lua_ls',
     'rust_analyzer',
+    'tsserver',
 })
 
+config.denols.setup {
+    root_dir = config.util.root_pattern("deno.json", "deno.jsonc"),
+}
 config.lua_ls.setup(lsp.nvim_lua_ls())
 config.rust_analyzer.setup({})
+config.tsserver.setup {
+    root_dir = config.util.root_pattern("package.json"),
+    single_file_support = false
+}
 
 -- Text completions
 local cmp_mappings = lsp.defaults.cmp_mappings({
