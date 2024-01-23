4ca0bd30ec51769bed3161a33b7a426430e61be4
Ethan Zimbelman <ethan.zimbelman@me.com>
2024-01-23 10:45:33 -0800

    nvim: ignore removal of a nonexistent file during setup

--- nix.sh
+++ nix.sh
@@ -38,7 +38,6 @@ function setup {
     nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
     nix-channel --update
     nix-shell '<home-manager>' -A install
-    rm result
     copy
 }
 
