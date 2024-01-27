d7a65674dfa5359f9ff47b53ea502ce3096e8ed4
Ethan Zimbelman <ethan.zimbelman@me.com>
2024-01-26 20:13:10 -0800

    eza: list directory contents in a stylish and ordered list

--- nix/home.nix
+++ nix/home.nix
@@ -15,6 +15,15 @@
       recursive = true;
     };
   };
+  home.sessionVariables = {
+    EZA_COLORS =
+      let
+        settingsList = builtins.attrNames config.programs.dircolors.settings;
+        formatKeyValue = k: "${k}=${builtins.getAttr k config.programs.dircolors.settings}";
+        settings = map formatKeyValue settingsList;
+      in
+        builtins.concatStringsSep ":" settings;
+  };
   programs.home-manager.enable = true;
   programs.zsh = {
     enable = true;
@@ -22,10 +31,45 @@
       PROMPT='%B%F{black}%(!.#.$)%b%f '
     '';
   };
+  programs.dircolors = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      "bu" = "38;5;181";
+      "da" = "2";
+      "di" = "38;5;73";
+      "ex" = "38;5;174";
+      "ln" = "38;5;224";
+      "nb" = "38;5;189";
+      "nk" = "38;5;188";
+      "nm" = "38;5;183";
+      "ng" = "38;5;140";
+      "nt" = "38;5;134";
+      "oc" = "2";
+      ".*" = "38;5;246";
+      "*.*" = "37";
+      "*.md" = "38;5;248";
+    };
+  };
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
+  };
+  programs.eza = {
+    enable = true;
+    enableAliases = true;
+    extraOptions = [
+      "--all"
+      "--classify"
+      "--group-directories-first"
+      "--long"
+      "--no-permissions"
+      "--no-user"
+      "--octal-permissions"
+      "--sort=Name"
+      "--time-style=long-iso"
+    ];
   };
   programs.neovim = {
     enable = true;
