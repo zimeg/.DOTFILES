be60e22853a040b18253909f2e8282e843e97da6
@zimeg <zim@o526.net>
2024-09-13 16:03:33 -0700

    ssh: attach to tmux sessions for started communication with tom

--- programs/ssh/default.nix
+++ programs/ssh/default.nix
@@ -11,6 +11,10 @@
         hostname = "192.168.86.37";
         identitiesOnly = true;
         identityFile = "~/.ssh/id_ed25519";
+        extraOptions = {
+          RemoteCommand = "tmux attach";
+          RequestTTY = "true";
+        };
       };
     };
   };
