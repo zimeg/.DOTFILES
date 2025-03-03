4543e6800b466fbf70bcf749f275a1e0cfc78571
@zimeg <eden.zimbelman@salesforce.com>
2025-03-03 10:00:45 -0800

    nix: match changes to avoid breaking builds on the work machine

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
               nixpkgs.overlays = [ nur.overlays.default ];
               home-manager = {
                 sharedModules = [ nur.modules.homeManager.default ];
-                useGlobalPkgs = true;
+                useGlobalPkgs = false; # https://github.com/zimeg/.DOTFILES/issues/29
                 useUserPackages = false;
                 users = {
                   "eden.zimbelman" = {
