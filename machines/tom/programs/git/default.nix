5b44961603124d885fa9ed96addb8598486881ca
@theorderingmachine <tom@deorr.co>
2026-03-28 23:38:54 -0700

    style: prefer matching homemanager implementation for allowed signers

--- machines/tom/programs/git/default.nix
+++ machines/tom/programs/git/default.nix
@@ -2,16 +2,5 @@
 {
   programs.git = {
     enable = true;
-    config = {
-      commit = {
-        gpgSign = true;
-      };
-      gpg = {
-        format = "ssh";
-      };
-      user = {
-        signingKey = "~/.ssh/id_ed25519";
-      };
-    };
   };
 }
