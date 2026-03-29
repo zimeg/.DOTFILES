9ab0466bd0984e0a391b3124fec3e93ac523ec63
Eden Zimbelman <eden.zimbelman@salesforce.com>
2026-03-28 23:08:27 -0700

    feat: show signatures on work machine too

--- machines/work/programs/git/default.nix
+++ machines/work/programs/git/default.nix
@@ -1,14 +1,22 @@
 # https://github.com/git/git
 {
   programs.git = {
-    signing = {
-      key = "~/.ssh/id_ed25519";
-    };
     settings = {
+      gpg = {
+        ssh = {
+          allowedSignersFile = "~/.config/git/allowed_signers";
+        };
+      };
       user = {
         email = "eden.zimbelman@salesforce.com";
         name = "Eden Zimbelman";
       };
     };
+    signing = {
+      key = "~/.ssh/id_ed25519";
+    };
   };
+  home.file.".config/git/allowed_signers".text = ''
+    eden.zimbelman@salesforce.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN56hpEg6OYyzNHNm5j8lGNYkE0nHYRIT5Tg1IJZk3oH
+  '';
 }
