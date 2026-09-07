bd0c9e3d7f4de57d95c36481636c9fece72aab63
@theorderingmachine <tom@deorr.co>
2026-09-07 13:48:59 -0700

    sops: read age key from persistent mount at boot

--- machines/tom/configuration.nix
+++ machines/tom/configuration.nix
@@ -208,7 +208,7 @@
     defaultSopsFormat = "yaml";
     age = {
       generateKey = false;
-      keyFile = "/var/lib/sops-nix/key.txt";
+      keyFile = "/persistent/var/lib/sops-nix/key.txt";
     };
     secrets = {
       "ai/credentials" = {
